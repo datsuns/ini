af3d7be27722617ac46acb2d7a5ecc8480adb2ea
datsuns <the.skeleton7@gmail.com>
2018-03-16 13:28:39 +0900

    add: file read

--- ini.go
+++ ini.go
@@ -1,10 +1,52 @@
 package ini
 
-import ()
+import (
+	"bufio"
+	//"errors"
+	"fmt"
+	"os"
+)
 
-type IniFile struct {
+type Entry struct {
+	key   string
+	value string
 }
 
-func Load(s string) (*IniFile, error) {
-	return nil, nil
+type Section struct {
+	name    string
+	entries []Entry
+}
+
+type File struct {
+	header   []string
+	sections []Section
+}
+
+func (f *File) Load(s string) error {
+	fd, err := os.Open(s)
+	if err != nil {
+		return err
+	}
+	defer fd.Close()
+	scanner := bufio.NewScanner(fd)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func NewFile(s string) (*File, error) {
+	ret := &File{}
+	err := ret.Load(s)
+	if err != nil {
+		return nil, err
+	}
+	return ret, nil
+}
+
+func Load(s string) (*File, error) {
+	return NewFile(s)
 }
