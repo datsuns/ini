e87cdceb3f5234a2d97e2cee79837ddfc21d1290
datsuns <the.skeleton7@gmail.com>
2018-04-09 13:57:53 +0900

    add: test case: test file header

--- ini_test.go
+++ ini_test.go
@@ -1,8 +1,33 @@
 package ini
 
 import (
+	"strings"
 	"testing"
 )
+
+func formatInput(list []string) string {
+	raw := strings.Join(list, "\n")
+	return raw
+}
+
+func TestKeepFileHeader(t *testing.T) {
+	text := []string{
+		"# ini file header is a text lines before the 1st section",
+		"dummy",
+		"a",
+		"[]",
+		";[]",
+	}
+	ini, err := LoadText(formatInput(text))
+	if err != nil {
+		t.Fatalf("load error [%v] %v", text, err)
+	}
+	for i, s := range ini.Header {
+		if s != text[i] {
+			t.Fatalf("invalid header [%v] != [%v]", text[i], s)
+		}
+	}
+}
 
 func TestParseSectionName(t *testing.T) {
 	list := map[string]string{
